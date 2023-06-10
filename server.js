4331f44b9f74fdb19b823b3abec48424b3ef1fba
Chu Nam <84580456+chuvannam02@users.noreply.github.com>
2023-06-10 16:48:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -57,8 +57,7 @@ app.listen(port, ()=> {
 })
 
 mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
   },
   (err) => {
     if (err) {
