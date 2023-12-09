1606c2236719532462333a398839db47770b362b
Nam Chu Van <proh2tbg2002@gmail.com>
2023-12-09 17:45:22 +0700

    Update server.js thay đổi cors thành link front-end deploy
    https://front-end-web-ecommerce.onrender.com/

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   // origin: "http://localhost:3001",
   // origin: "http://127.0.0.1:5173",
-  origin: "http://localhost:5173",
+ // origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
-
+  origin:'https://front-end-web-ecommerce.onrender.com',
   // origin: 'https://mern-stack-frontend.onrender.com',
   credentials: true,
 };
